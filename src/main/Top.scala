beedd307a34defff2213b0f61ecd3793366d454e
lzzsG <lizizhi122@gmail.com>
2024-12-14 22:04:16 +0800

    Practice commit 2024-12-14 22:04:16

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -8,21 +8,20 @@ import _root_.circt.stage.ChiselStage
 class Top extends Module {
   val io = IO(new Bundle {
 // start
-    val a               = Input(Bool())
-    val b               = Input(Bool())
-    val out_assign      = Output(Bool())
-    val out_always_comb = Output(Bool())
-    val out_always_ff   = Output(Bool())
-// end
+    val a          = Input(Bool())
+    val b          = Input(Bool())
+    val sel_b1     = Input(Bool())
+    val sel_b2     = Input(Bool())
+    val out_assign = Output(Bool())
+    val out_always = Output(Bool())
   })
 
-  val aXorb = Wire(Bool())
-  aXorb := io.a ^ io.b
+  io.out_assign := Mux(io.sel_b1 && io.sel_b2, io.b, io.a)
 
-  io.out_assign := aXorb
-
-  io.out_always_comb := aXorb
-
-  io.out_always_ff := RegNext(aXorb)
+  when(io.sel_b1 && io.sel_b2) {
+    io.out_always := io.b
+  }.otherwise {
+    io.out_always := io.a
+  }
 
 }
