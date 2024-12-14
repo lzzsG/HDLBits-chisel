3896e71e7063534714304596e94c712b2149bb17
lzzsG <lizizhi122@gmail.com>
2024-12-14 15:50:21 +0800

    Practice commit 2024-12-14 15:50:21

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -8,10 +8,12 @@ import _root_.circt.stage.ChiselStage
 class Top extends Module {
   val io = IO(new Bundle {
 
-// start
-    val
+    val in     = Input(UInt(16.W))
+    val out_hi = Output(UInt(8.W))
+    val out_lo = Output(UInt(8.W))
 
   })
 
-
+  io.out_hi := io.in(15, 8)
+  io.out_lo := io.in(7, 0)
 }
