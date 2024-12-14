e37e1054d816d87eb087fa6fae8c24bc01a8348a
lzzsG <lizizhi122@gmail.com>
2024-12-14 18:18:33 +0800

    Practice commit 2024-12-14 18:18:33

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -3,15 +3,15 @@ package top
 import chisel3._
 import _root_.circt.stage.ChiselStage
 
-class mod_a extends Module {
+class mod_a extends BlackBox {
   val io = IO(new Bundle {
-    val in1 = Input(Bool())
-    val in2 = Input(Bool())
-    val out = Output(Bool())
+    val out1 = Output(Bool())
+    val out2 = Output(Bool())
+    val a    = Input(Bool())
+    val b    = Input(Bool())
+    val c    = Input(Bool())
+    val d    = Input(Bool())
   })
-
-  // 子模块逻辑（假设是 AND 门，仅作说明）
-  io.out := io.in1 & io.in2
 }
 
 /** Top module
@@ -20,16 +20,22 @@ class Top extends Module {
   val io = IO(new Bundle {
 
 // start
-    val a   = Input(Bool())
-    val b   = Input(Bool())
-    val out = Output(Bool())
+    val a    = Input(Bool())
+    val b    = Input(Bool())
+    val c    = Input(Bool())
+    val d    = Input(Bool())
+    val out1 = Output(Bool())
+    val out2 = Output(Bool())
 
   })
 
-  val u_mode_a = Module(new mod_a)
+  val u_mod_a = Module(new mod_a)
 
-  u_mode_a.io.in1 := io.a
-  u_mode_a.io.in2 := io.b
-  io.out          := u_mode_a.io.out
+  io.out1      := u_mod_a.io.out1
+  io.out2      := u_mod_a.io.out2
+  u_mod_a.io.a := io.a
+  u_mod_a.io.b := io.b
+  u_mod_a.io.c := io.c
+  u_mod_a.io.d := io.d
 
 }
