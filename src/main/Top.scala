adcaab03229a48fcb00959552cfc8eeefdae2456
lzzsG <lizizhi122@gmail.com>
2024-12-14 16:50:42 +0800

    Practice commit 2024-12-14 16:50:42

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -2,8 +2,17 @@ package top
 
 import chisel3._
 import _root_.circt.stage.ChiselStage
-import chisel3.util.Fill
-import chisel3.util.Cat
+
+class mod_a extends Module {
+  val io = IO(new Bundle {
+    val in1 = Input(Bool())
+    val in2 = Input(Bool())
+    val out = Output(Bool())
+  })
+
+  // 子模块逻辑（假设是 AND 门，仅作说明）
+  io.out := io.in1 & io.in2
+}
 
 /** Top module
   */
@@ -13,16 +22,14 @@ class Top extends Module {
 // start
     val a   = Input(Bool())
     val b   = Input(Bool())
-    val c   = Input(Bool())
-    val d   = Input(Bool())
-    val e   = Input(Bool())
-    val out = Output(UInt(25.W))
+    val out = Output(Bool())
 
   })
 
-  val repeated_mix  = Fill(5, Cat(io.a, io.b, io.c, io.d, io.e))
-  val repeated_same = Cat(Fill(5, io.a), Fill(5, io.b), Fill(5, io.c), Fill(5, io.d), Fill(5, io.e))
+  val u_mode_a = Module(new mod_a)
 
-  io.out := ~(repeated_mix ^ repeated_same)
+  u_mode_a.io.in1 := io.a
+  u_mode_a.io.in2 := io.b
+  io.out          := u_mode_a.io.out
 
 }
