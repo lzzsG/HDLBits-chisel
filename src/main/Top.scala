0152ed645bbb79e788e5b6f5305f367df85f9ca7
lzzsG <lizizhi122@gmail.com>
2024-12-14 15:52:18 +0800

    Practice commit 2024-12-14 15:52:18

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -2,18 +2,19 @@ package top
 
 import chisel3._
 import _root_.circt.stage.ChiselStage
+import chisel3.util.Cat
 
 /** Top module
   */
 class Top extends Module {
   val io = IO(new Bundle {
 
-    val in     = Input(UInt(16.W))
-    val out_hi = Output(UInt(8.W))
-    val out_lo = Output(UInt(8.W))
+// start
+    val in  = Input(UInt(32.W))
+    val out = Output(UInt(32.W))
 
   })
 
-  io.out_hi := io.in(15, 8)
-  io.out_lo := io.in(7, 0)
+  io.out := Cat(io.in(7, 0), io.in(15, 8), io.in(23, 16), io.in(31, 24))
+
 }
