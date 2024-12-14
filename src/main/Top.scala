7ca4a8ecb505848b2476b87533b5bed2c7918438
lzzsG <lizizhi122@gmail.com>
2024-12-14 16:16:13 +0800

    Practice commit 2024-12-14 16:16:13

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -2,6 +2,7 @@ package top
 
 import chisel3._
 import _root_.circt.stage.ChiselStage
+import chisel3.util.Cat
 
 /** Top module
   */
@@ -9,15 +10,24 @@ class Top extends Module {
   val io = IO(new Bundle {
 
 // start
-    val in      = Input(UInt(4.W))
-    val out_and = Output(Bool())
-    val out_or  = Output(Bool())
-    val out_xor = Output(Bool())
+    val a = Input(UInt(5.W))
+    val b = Input(UInt(5.W))
+    val c = Input(UInt(5.W))
+    val d = Input(UInt(5.W))
+    val e = Input(UInt(5.W))
+    val f = Input(UInt(5.W))
+    val w = Output(UInt(8.W))
+    val x = Output(UInt(8.W))
+    val y = Output(UInt(8.W))
+    val z = Output(UInt(8.W))
 
   })
 
-  io.out_and := io.in.andR
-  io.out_or  := io.in.orR
-  io.out_xor := io.in.xorR
+  val connected = Cat(io.a, io.b, io.c, io.d, io.e, io.f, 1.U(1.W), 1.U(1.W))
+
+  io.w := connected(31, 24)
+  io.x := connected(23, 16)
+  io.y := connected(15, 8)
+  io.z := connected(7, 0)
 
 }
