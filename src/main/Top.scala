52130572a88553aef37b9bf95d5d287ec586b812
lzzsG <lizizhi122@gmail.com>
2024-12-14 16:32:38 +0800

    Practice commit 2024-12-14 16:32:38

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -2,8 +2,8 @@ package top
 
 import chisel3._
 import _root_.circt.stage.ChiselStage
-import chisel3.util.Cat
 import chisel3.util.Fill
+import chisel3.util.Cat
 
 /** Top module
   */
@@ -11,11 +11,18 @@ class Top extends Module {
   val io = IO(new Bundle {
 
 // start
-    val in  = Input(UInt(8.W))
-    val out = Output(UInt(32.W))
+    val a   = Input(Bool())
+    val b   = Input(Bool())
+    val c   = Input(Bool())
+    val d   = Input(Bool())
+    val e   = Input(Bool())
+    val out = Output(UInt(25.W))
 
   })
 
-  io.out := Cat(Fill(24, io.in(7)), io.in)
+  val repeated_mix  = Fill(5, Cat(io.a, io.b, io.c, io.d, io.e))
+  val repeated_same = Cat(Fill(5, io.a), Fill(5, io.b), Fill(5, io.c), Fill(5, io.d), Fill(5, io.e))
+
+  io.out := ~(repeated_mix ^ repeated_same)
 
 }
