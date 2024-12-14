db7160ea34a6a3574052b68fcb86c4d66ea1e0d3
lzzsG <lizizhi122@gmail.com>
2024-12-14 21:46:45 +0800

    Practice commit 2024-12-14 21:46:45

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -1,49 +1,27 @@
 package top
 
 import chisel3._
-import chisel3.util._
 import _root_.circt.stage.ChiselStage
-
-class add16 extends Module {
-  val io     = IO(new Bundle {
-    val a    = Input(UInt(16.W))
-    val b    = Input(UInt(16.W))
-    val cin  = Input(Bool())
-    val sum  = Output(UInt(16.W))
-    val cout = Output(Bool())
-  })
-  val result = io.a +& io.b +& io.cin
-  io.sum  := result(15, 0)
-  io.cout := result(16)
-}
 
 /** Top module
   */
 class Top extends Module {
   val io = IO(new Bundle {
 // start
-    val a   = Input(UInt(32.W))
-    val b   = Input(UInt(32.W))
-    val sub = Input(Bool())
-    val sum = Output(UInt(32.W))
+    val a               = Input(Bool())
+    val b               = Input(Bool())
+    val out_assign      = Output(Bool())
+    val out_alwaysblock = Output(Bool())
   })
 
-  val inverted_b = io.b ^ Fill(32, io.sub)
-  val loSum      = Wire(UInt(16.W))
-  val hiSum      = Wire(UInt(16.W))
+  io.out_assign := io.a & io.b
 
-  val adder0 = Module(new add16)
-  adder0.io.a   := io.a(15, 0)
-  adder0.io.b   := inverted_b(15, 0)
-  adder0.io.cin := io.sub
-  loSum         := adder0.io.sum
-
-  val adder1 = Module(new add16)
-  adder1.io.a   := io.a(31, 16)
-  adder1.io.b   := inverted_b(31, 16)
-  adder1.io.cin := adder0.io.cout
-  hiSum         := adder1.io.sum
-
-  io.sum := Cat(hiSum, loSum)
+  val result = Wire(Bool())
+  when(io.a && io.b) {
+    result := true.B
+  }.otherwise {
+    result := false.B
+  }
+  io.out_alwaysblock := result
 
 }
