9c60b6bf473edc6335788ce31566438ae12f64b0
lzzsG <lizizhi122@gmail.com>
2024-12-14 19:37:57 +0800

    Practice commit 2024-12-14 19:37:57

--- src/main/Top.scala
+++ src/main/Top.scala
@@ -2,17 +2,21 @@ package top
 
 import chisel3._
 import _root_.circt.stage.ChiselStage
-import chisel3.util.MuxLookup
+import chisel3.util.Cat
 
-class my_diff8 extends Module {
+class add16 extends Module {
   val io = IO(new Bundle {
-    val clk = Input(Clock())
-    val d   = Input(UInt(8.W))
-    val q   = Output(UInt(8.W))
+    val a    = Input(UInt(16.W))
+    val b    = Input(UInt(16.W))
+    val cin  = Input(UInt(1.W))
+    val cout = Output(UInt(1.W))
+    val sum  = Output(UInt(16.W))
   })
 
-  val q = RegNext(io.d, 0.U)
-  io.q := q
+  val result = Wire(UInt(17.W))
+  result  := io.a + io.b + io.cin
+  io.sum  := result(15, 0)
+  io.cout := result(16)
 }
 
 /** Top module
@@ -21,40 +25,24 @@ class Top extends Module {
   val io = IO(new Bundle {
 
 // start
-    val clk = Input(Clock())
-    val sel = Input(UInt(2.W))
-    val d   = Input(UInt(8.W))
-    val q   = Output(UInt(8.W))
-
+    val a   = Input(UInt(32.W))
+    val b   = Input(UInt(32.W))
+    val sum = Output(UInt(32.W))
   })
 
-  val q1 = Wire(UInt(8.W))
-  val q2 = Wire(UInt(8.W))
-  val q3 = Wire(UInt(8.W))
+  val loCout = Wire(UInt(1.W))
 
-  val u_diff1 = Module(new my_diff8)
-  val u_diff2 = Module(new my_diff8)
-  val u_diff3 = Module(new my_diff8)
+  val add16_0 = Module(new add16)
+  add16_0.io.a   := io.a(15, 0)
+  add16_0.io.b   := io.b(15, 0)
+  add16_0.io.cin := false.B
+  loCout         := add16_0.io.cout
 
-  u_diff1.io.clk := io.clk
-  u_diff1.io.d   := io.d
-  q1             := u_diff1.io.q
+  val add16_1 = Module(new add16)
+  add16_1.io.a   := io.a(31, 16)
+  add16_1.io.b   := io.b(31, 16)
+  add16_1.io.cin := loCout
 
-  u_diff2.io.clk := io.clk
-  u_diff2.io.d   := q1
-  q2             := u_diff2.io.q
-
-  u_diff3.io.clk := io.clk
-  u_diff3.io.d   := q2
-  q3             := u_diff3.io.q
-
-  io.q := MuxLookup(io.sel, io.d)(
-    Seq(
-      0.U -> io.d,
-      1.U -> q1,
-      2.U -> q2,
-      3.U -> q3
-    )
-  )
+  io.sum := Cat(add16_1.io.sum, add16_0.io.sum)
 
 }
